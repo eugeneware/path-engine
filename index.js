f8378c04c9be817e336c9b2c49374ee89544ebc3
Eugene Ware <eugene@noblesamurai.com>
2013-07-29 22:12:58 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+module.exports = pathEngine;
+function pathEngine() {
+  return {
+    query: query,
+    match: match,
+    plans: {
+      'property': propertyPlan,
+      'pairs': pairsPlan
+    }
+  };
+}
+
+function parseQuery(q) {
+  return {
+    prop: q.slice(0, q.length - 1).join('.'),
+    path: q.slice(0, q.length - 1),
+    value: q[q.length - 1]
+  };
+}
+
+function propertyPlan(idx, queryParts) {
+  var db = this;
+  return db.indexes[idx].createIndexStream({
+    start: [queryParts.value, null],
+    end: [queryParts.value, undefined]
+  });
+}
+
+function pairsPlan(idx, queryParts) {
+  var db = this;
+  var stem = queryParts.path.concat(queryParts.value);
+  if (stem.length > 2) {
+    stem = stem.slice(stem.length - 2);
+  }
+  return db.indexes[idx].createIndexStream({
+    start: stem.concat(null),
+    end: stem.concat(undefined)
+  });
+}
+
+function query(q) {
+  var db = this;
+  var queryParts = parseQuery(q);
+  var idx = db.indexes[queryParts.prop];
+  if (idx && idx.type in db.query.engine.plans) {
+    return db.query.engine.plans[idx.type].call(db, queryParts.prop, queryParts);
+  } else if ((idx = db.indexes['*']) && idx.type in db.query.engine.plans) {
+    return db.query.engine.plans[idx.type].call(db, '*', queryParts);
+  } else {
+    return null;
+  }
+}
+
+function match(q, obj) {
+  var queryParts = parseQuery(q);
+  return fetchProp(obj, queryParts.path) === queryParts.value;
+}
+
+function fetchProp(obj, path) {
+  while (path.length > 0) {
+    var prop = path.shift();
+    if (obj[prop] !== undefined) {
+      obj = obj[prop];
+    } else {
+      return;
+    }
+  }
+  return obj;
+}
---
+++ test/level-plan.js
@@ -0,0 +1,164 @@
+var expect = require('chai').expect,
+    bytewise = require('byteup')(),
+    levelup = require('levelup'),
+    path = require('path'),
+    pairs = require('pairs'),
+    levelQuery = require('level-queryengine'),
+    pathEngine = require('..'),
+    rimraf = require('rimraf'),
+    after = require('after');
+
+function encode(key) {
+  return bytewise.encode(key).toString('hex');
+}
+
+function decode(key) {
+  return bytewise.decode(new Buffer(key, 'hex'));
+}
+
+function log() {
+  console.error.apply(console, [].slice.apply(arguments));
+}
+
+describe('level-plan', function() {
+  var db, dbPath = path.join(__dirname, '..', 'data', 'test-db');
+
+  beforeEach(function(done) {
+    rimraf.sync(dbPath);
+    db = levelup(dbPath, { keyEncoding: 'bytewise', valueEncoding: 'json' }, done);
+  });
+
+  afterEach(function(done) {
+    db.close(done);
+  });
+
+  it('should be able to build a simple path query engine', function(done) {
+    db = levelQuery(db);
+
+    db.query.use(pathEngine());
+    db.ensureIndex('name');
+
+    db.batch(testData(), doQuery);
+    function doQuery(err) {
+      if (err) return done(err);
+      var hits = 0;
+      db.query(['name', 'name 42'])
+        .on('data', function (data) {
+          hits++;
+          expect(data.name).to.equal('name 42');
+          expect(data.num).to.equal(420);
+        })
+        .on('stats', function (stats) {
+          expect(stats).to.deep.equals(
+            { indexHits: 1, dataHits: 1, matchHits: 1 });
+        })
+        .on('end', function () {
+          expect(hits).to.equal(1);
+          done();
+        });
+    }
+  });
+
+  it('should be able to be able to do a table scan when no index matches', function(done) {
+    db = levelQuery(db);
+
+    db.query.use(pathEngine());
+    db.ensureIndex('name');
+
+    db.batch(testData(), doQuery);
+    function doQuery(err) {
+      if (err) return done(err);
+      var hits = 0;
+      db.query(['num', 420])
+        .on('data', function (data) {
+          hits++;
+        })
+        .on('stats', function (stats) {
+          expect(stats).to.deep.equals(
+            { indexHits: 0, dataHits: 100, matchHits: 1 });
+        })
+        .on('end', function () {
+          expect(hits).to.equal(1);
+          done();
+        });
+    }
+  });
+
+  it('should be able to use the path engines with pairs', function(done) {
+    db = levelQuery(db);
+
+    db.query.use(pathEngine());
+    db.ensureIndex('*', 'pairs', function (key, value, emit) {
+      if (value && typeof value === 'object' && Object.keys(value).length > 0) {
+        pairs(value).forEach(function (pair) {
+          emit(pair);
+        });
+      }
+    });
+
+    db.batch(testData(), doQuery);
+    function doQuery(err) {
+      if (err) return done(err);
+      var hits = 0;
+      db.query(['car', 'year', 2035])
+        .on('data', function (data) {
+          hits++;
+          expect(data.name).to.equal('name 42');
+          expect(data.num).to.equal(420);
+        })
+        .on('stats', function (stats) {
+          expect(stats).to.deep.equals(
+            { indexHits: 1, dataHits: 1, matchHits: 1 });
+        })
+        .on('end', function () {
+          expect(hits).to.equal(1);
+          done();
+        });
+    }
+  });
+});
+
+function testData() {
+  var batch = [];
+  for (var i = 0; i < 100; i++) {
+    var obj = {
+      name: 'name ' + i,
+      car: {
+        make: 'Toyota',
+        model: i % 2 ? 'Camry' : 'Corolla',
+        year: 1993 + i
+      },
+      pets: [
+        { species: 'Cat', breed: i == 50 ? 'Saimese' : 'Burmese' },
+        { species: 'Cat', breed: 'DSH' },
+        { species: 'Dog', breed: 'Dalmation' }
+      ],
+      tags: [
+        'tag1', 'tag2', 'tag3'
+      ],
+      tagsNoIndex: [
+        'tag1', 'tag2', 'tag3'
+      ],
+      tree: {
+        a: i,
+        b: i + 1,
+      },
+      treeNoIndex: {
+        a: i,
+        b: i + 1,
+      },
+      num: 10*i,
+      numNoIndex: 10*i
+    };
+    if (i === 42) {
+      obj.tags.push('tag4');
+      obj.tagsNoIndex.push('tag4');
+    }
+    if (i === 84) {
+      delete obj.name;
+    }
+    batch.push({ type: 'put', key: i, value: obj });
+  }
+
+  return batch;
+}
